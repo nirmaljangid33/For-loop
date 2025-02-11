098fefbffd2ad0d45e2158d8f8b1931e52745ea9
nirmal jangid <jangidnirmal15@gmail.com>
2025-02-11 14:46:13 +0530

    100--->1 number print

---
+++ A6.java
@@ -0,0 +1,11 @@
+public class A6 {
+    
+        public static void main(String[] args) {
+            for(int i=100; i>=1;--i){
+                System.out.println(i);
+            }
+        }
+    }
+
+
+
