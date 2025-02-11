09f016712c91e745918686ae6b4056aa0bb9cf8e
nirmal jangid <jangidnirmal15@gmail.com>
2025-02-11 13:01:18 +0530

    sum of first 10 natural number

---
+++ A2.java
@@ -0,0 +1,10 @@
+public class A2 {
+    public static void main(String[] args){
+        int sum =0;
+        for(int i=1; i<=10;i++){
+            sum +=i;
+        }
+        System.out.println("Number of sum = " +sum );
+
+    }
+}
